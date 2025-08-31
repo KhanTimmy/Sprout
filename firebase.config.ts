21c71bdeb9e2b7c147a10ea115912cb3e5c55f49
jshummel <149720797+jshummel@users.noreply.github.com>
2025-08-31 09:16:04 -0500

    updated firebase config

--- firebase.config.ts
+++ firebase.config.ts
@@ -2,21 +2,18 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import Constants from 'expo-constants';
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD6VYt_8KSDjOLg3gSFPgn8BwCWj9erQoI",
-  authDomain: "sprout-ed446.firebaseapp.com",
-  projectId: "sprout-ed446",
-  storageBucket: "sprout-ed446.firebasestorage.app",
-  messagingSenderId: "792033994499",
-  appId: "1:792033994499:web:83a7036b8eef018598eb36"
+  apiKey: Constants.expoConfig?.extra?.firebaseApiKey,
+  authDomain: Constants.expoConfig?.extra?.firebaseAuthDomain,
+  projectId: Constants.expoConfig?.extra?.firebaseProjectId,
+  storageBucket: Constants.expoConfig?.extra?.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig?.extra?.firebaseMessagingSenderId,
+  appId: Constants.expoConfig?.extra?.firebaseAppId,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage)
